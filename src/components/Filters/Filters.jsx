88dbb7e5fc3b22b0c3cdd6189b1931dff6748ebc
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-09-16 01:03:13 +0300

    Moved Filters to component.

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,198 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setLocation,
+  setType,
+  setHasAC,
+  setHasKitchen,
+  setHasBathroom,
+  setHasTV,
+  setTransmission,
+} from "../../redux/filtersSlice";
+import styles from "./Filters.module.css";
+import { SVG } from "../../components/svg/svg";
+
+const Filters = () => {
+    const dispatch = useDispatch();
+    const filters = useSelector((state) => state.filters);
+  // Handle filter changes
+  const handleFilterChange = (event) => {
+    const { name, value, checked, type } = event.target;
+    if (type === "checkbox") {
+      switch (name) {
+        case "hasAC":
+          dispatch(setHasAC(checked));
+          break;
+        case "hasKitchen":
+          dispatch(setHasKitchen(checked));
+          break;
+        case "hasBathroom":
+          dispatch(setHasBathroom(checked));
+          break;
+        case "hasTV":
+          dispatch(setHasTV(checked));
+          break;
+        case "isAutomatic":
+          dispatch(setTransmission(checked ? "automatic" : ""));
+          break;
+        default:
+          break;
+      }
+    } else {
+      dispatch(name === "location" ? setLocation(value) : setType(value));
+    }
+  };
+
+  return (
+    <div className={styles.filtersSection}>
+      <div className={styles.locationLabel}>Location</div>
+      <div className={styles.locationInputContainer}>
+        <input
+          type="text"
+          name="location"
+          placeholder="Location"
+          value={filters.location}
+          onChange={handleFilterChange}
+          className={styles.locationInput}
+        />
+        <SVG id="map" className={styles.inputIcon} />
+      </div>
+
+      <div className={styles.filtersLabel}>Filters</div>
+
+      <div className={styles.filterGroup}>
+        <div className={styles.filterTitle}>Vehicle equipment</div>
+        <div className={styles.checkboxGroup}>
+          <label
+            className={`${styles.checkboxLabel} ${
+              filters.hasAC ? styles.checkboxLabelActive : ""
+            }`}
+          >
+            <SVG id="wind" width={32} height={32} />
+            <input
+              type="checkbox"
+              name="hasAC"
+              checked={filters.hasAC}
+              onChange={handleFilterChange}
+            />
+            AC
+          </label>
+          <label
+            className={`${styles.checkboxLabel} ${
+              filters.transmission === "automatic"
+                ? styles.checkboxLabelActive
+                : ""
+            }`}
+          >
+            <SVG id="diagram" width={32} height={32} />
+            <input
+              type="checkbox"
+              name="isAutomatic"
+              checked={filters.transmission === "automatic"}
+              onChange={handleFilterChange}
+            />
+            Automatic
+          </label>
+          <label
+            className={`${styles.checkboxLabel} ${
+              filters.hasKitchen ? styles.checkboxLabelActive : ""
+            }`}
+          >
+            <SVG id="cup-hot" width={32} height={32} />
+            <input
+              type="checkbox"
+              name="hasKitchen"
+              checked={filters.hasKitchen}
+              onChange={handleFilterChange}
+            />
+            Kitchen
+          </label>
+          <label
+            className={`${styles.checkboxLabel} ${
+              filters.hasTV ? styles.checkboxLabelActive : ""
+            }`}
+          >
+            <SVG id="tv" width={32} height={32} />
+            <input
+              type="checkbox"
+              name="hasTV"
+              checked={filters.hasTV}
+              onChange={handleFilterChange}
+            />
+            TV
+          </label>
+          <label
+            className={`${styles.checkboxLabel} ${
+              filters.hasBathroom ? styles.checkboxLabelActive : ""
+            }`}
+          >
+            <SVG id="bi-droplet" width={32} height={32} />
+            <input
+              type="checkbox"
+              name="hasBathroom"
+              checked={filters.hasBathroom}
+              onChange={handleFilterChange}
+            />
+            Bathroom
+          </label>
+        </div>
+      </div>
+
+      <div className={styles.filterGroup}>
+        <div className={styles.filterTitle}>Vehicle type</div>
+
+        <div className={styles.checkboxGroup}>
+          <label
+            className={`${styles.checkboxLabel} ${styles.checkboxLabelLong} ${
+              filters.type === "panelTruck" ? styles.checkboxLabelActive : ""
+            }`}
+          >
+            <SVG id="bi-grid-1x2" width={32} height={32} />
+            <input
+              type="radio"
+              name="type"
+              value="panelTruck"
+              checked={filters.type === "panelTruck"}
+              onChange={handleFilterChange}
+            />
+            Panel Truck
+          </label>
+          <label
+            className={`${styles.checkboxLabel} ${styles.checkboxLabelLong} ${
+              filters.type === "fullyIntegrated"
+                ? styles.checkboxLabelActive
+                : ""
+            }`}
+          >
+            <SVG id="bi-grid" width={32} height={32} />
+            <input
+              type="radio"
+              name="type"
+              value="fullyIntegrated"
+              checked={filters.type === "fullyIntegrated"}
+              onChange={handleFilterChange}
+            />
+            Fully integrated
+          </label>
+          <label
+            className={`${styles.checkboxLabel} ${
+              filters.type === "alcove" ? styles.checkboxLabelActive : ""
+            }`}
+          >
+            <SVG id="bi-grid-3x3-gap" width={32} height={32} />
+            <input
+              type="radio"
+              name="type"
+              value="alcove"
+              checked={filters.type === "alcove"}
+              onChange={handleFilterChange}
+            />
+            Alcove
+          </label>
+        </div>
+      </div>
+      {/*<button className={styles.showMoreButton}>Search</button>*/}
+    </div>
+  );
+};
+
+export default Filters;
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -6,13 +6,6 @@ import {
   resetFilteredList,
 } from "../../redux/campersSlice";
 import {
-  setLocation,
-  setType,
-  setHasAC,
-  setHasKitchen,
-  setHasBathroom,
-  setHasTV,
-  setTransmission,
   resetFilters,
 } from "../../redux/filtersSlice";
 import {
@@ -24,6 +17,7 @@ import Loader from "../../components/Loader/Loader";
 import styles from "./CatalogPage.module.css";
 
 import { SVGSource, SVG } from "../../components/svg/svg";
+import Filters from "../../components/Filters/Filters";
 
 
 const CatalogPage = () => {
@@ -78,34 +72,6 @@ const CatalogPage = () => {
     setPage(1);
   }, [filters, dispatch, campers]);
 
-  // Handle filter changes
-  const handleFilterChange = (event) => {
-    const { name, value, checked, type } = event.target;
-    if (type === "checkbox") {
-      switch (name) {
-        case "hasAC":
-          dispatch(setHasAC(checked));
-          break;
-        case "hasKitchen":
-          dispatch(setHasKitchen(checked));
-          break;
-        case "hasBathroom":
-          dispatch(setHasBathroom(checked));
-          break;
-        case "hasTV":
-          dispatch(setHasTV(checked));
-          break;
-        case "isAutomatic":
-          dispatch(setTransmission(checked ? "automatic" : ""));
-          break;
-        default:
-          break;
-      }
-    } else {
-      dispatch(name === "location" ? setLocation(value) : setType(value));
-    }
-  };
-
   // Load more campers
   const loadMore = () => {
     setPage((prevPage) => prevPage + 1);
@@ -116,7 +82,7 @@ const CatalogPage = () => {
     0,
     page * itemsPerPage
   );
-  
+
   // Handle favorites
   const getIsFavorite = (id) => {
     return favorites.includes(id);
@@ -132,159 +98,20 @@ const CatalogPage = () => {
   
   return (
     <div className={styles.catalogPage}>
-      <div className={styles.filtersSection}>
-        <div className={styles.locationLabel}>Location</div>
-        <div className={styles.locationInputContainer}>
-          <input
-            type="text"
-            name="location"
-            placeholder="Location"
-            value={filters.location}
-            onChange={handleFilterChange}
-            className={styles.locationInput}
-          />
-          <SVG id="map" className={styles.inputIcon} />
-        </div>
-
-        <div className={styles.filtersLabel}>Filters</div>
-
-        <div className={styles.filterGroup}>
-          <div className={styles.filterTitle}>Vehicle equipment</div>
-          <div className={styles.checkboxGroup}>
-            <label
-              className={`${styles.checkboxLabel} ${
-                filters.hasAC ? styles.checkboxLabelActive : ""
-              }`}
-            >
-              <SVG id="wind" width={32} height={32} />
-              <input
-                type="checkbox"
-                name="hasAC"
-                checked={filters.hasAC}
-                onChange={handleFilterChange}
-              />
-              AC
-            </label>
-            <label
-              className={`${styles.checkboxLabel} ${
-                filters.transmission === "automatic"
-                  ? styles.checkboxLabelActive
-                  : ""
-              }`}
-            >
-              <SVG id="diagram" width={32} height={32} />
-              <input
-                type="checkbox"
-                name="isAutomatic"
-                checked={filters.transmission === "automatic"}
-                onChange={handleFilterChange}
-              />
-              Automatic
-            </label>
-            <label
-              className={`${styles.checkboxLabel} ${
-                filters.hasKitchen ? styles.checkboxLabelActive : ""
-              }`}
-            >
-              <SVG id="cup-hot" width={32} height={32} />
-              <input
-                type="checkbox"
-                name="hasKitchen"
-                checked={filters.hasKitchen}
-                onChange={handleFilterChange}
-              />
-              Kitchen
-            </label>
-            <label
-              className={`${styles.checkboxLabel} ${
-                filters.hasTV ? styles.checkboxLabelActive : ""
-              }`}
-            >
-              <SVG id="tv" width={32} height={32} />
-              <input
-                type="checkbox"
-                name="hasTV"
-                checked={filters.hasTV}
-                onChange={handleFilterChange}
-              />
-              TV
-            </label>
-            <label
-              className={`${styles.checkboxLabel} ${
-                filters.hasBathroom ? styles.checkboxLabelActive : ""
-              }`}
-            >
-              <SVG id="bi-droplet" width={32} height={32} />
-              <input
-                type="checkbox"
-                name="hasBathroom"
-                checked={filters.hasBathroom}
-                onChange={handleFilterChange}
-              />
-              Bathroom
-            </label>
-          </div>
-        </div>
-
-        <div className={styles.filterGroup}>
-          <div className={styles.filterTitle}>Vehicle type</div>
-
-          <div className={styles.checkboxGroup}>
-            <label
-              className={`${styles.checkboxLabel} ${styles.checkboxLabelLong} ${
-                filters.type === "panelTruck" ? styles.checkboxLabelActive : ""
-              }`}
-            >
-              <SVG id="bi-grid-1x2" width={32} height={32} />
-              <input
-                type="radio"
-                name="type"
-                value="panelTruck"
-                checked={filters.type === "panelTruck"}
-                onChange={handleFilterChange}
-              />
-              Panel Truck
-            </label>
-            <label
-              className={`${styles.checkboxLabel} ${styles.checkboxLabelLong} ${
-                filters.type === "fullyIntegrated"
-                  ? styles.checkboxLabelActive
-                  : ""
-              }`}
-            >
-              <SVG id="bi-grid" width={32} height={32} />
-              <input
-                type="radio"
-                name="type"
-                value="fullyIntegrated"
-                checked={filters.type === "fullyIntegrated"}
-                onChange={handleFilterChange}
-              />
-              Fully integrated
-            </label>
-            <label
-              className={`${styles.checkboxLabel} ${
-                filters.type === "alcove" ? styles.checkboxLabelActive : ""
-              }`}
-            >
-              <SVG id="bi-grid-3x3-gap" width={32} height={32} />
-              <input
-                type="radio"
-                name="type"
-                value="alcove"
-                checked={filters.type === "alcove"}
-                onChange={handleFilterChange}
-              />
-              Alcove
-            </label>
-          </div>
-        </div>
-        {/*<button className={styles.showMoreButton}>Search</button>*/}
-      </div>
+      <Filters />
 
       <div className={styles.campersList}>
         {status === "loading" && <Loader />}
-        {status === "succeeded" && displayedCampers &&
+        {status === "succeeded" && !displayedCampers && (
+          <p className={styles.noCampersFound}>No campers found</p>
+        )}
+
+        {status === "failed" && (
+          <p className={styles.noCampersFound}>Error loading campers</p>
+        )}
+
+        {status === "succeeded" &&
+          displayedCampers &&
           displayedCampers.map((camper) => (
             <div key={camper.id} className={styles.camperCard}>
               <img src={camper.gallery[0].thumb} alt={camper.name} />
@@ -393,12 +220,6 @@ const CatalogPage = () => {
               </div>
             </div>
           ))}
-        {status === "succeeded" && displayedCampers.length === 0 && (
-          <p className={styles.noCampersFound}>No campers found</p>
-        )}
-        {status === "failed" && (
-          <p className={styles.noCampersFound}>Error loading campers</p>
-        )}
 
         {status === "succeeded" &&
           displayedCampers.length < filteredCampers.length && (
